b352059efa9743a9941db08d750f144f82747cbe
Jack Tinker <jackrtinker@gmail.com>
2025-04-15 15:41:21 -0400

    Added ipynb

--- code/analysis.py
+++ code/analysis.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 test_cols = pd.read_csv("code/data/test.csv")
 test_preds = pd.read_csv("code/submissions/submission.csv")
 
+
+print("Number of NaNs:", test_cols.isna().sum()[test_cols.isna().sum() != 0])
+
 test_w_preds = pd.concat([test_cols, test_preds], axis = 1)
 
 print(test_w_preds.head())
 
 plt.scatter(test_w_preds["Ins_Age"], test_w_preds["Response"])
-plt.show()
+#plt.show()
 
